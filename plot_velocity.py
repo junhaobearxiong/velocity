a932d91d5c5494d5ddba05849e327078164542a6
junhaobearxiong <jxiong3@jhu.edu>
2021-02-21 15:21:16 +0800

    change plot settings

--- plot_velocity.py
+++ plot_velocity.py
@@ -9,14 +9,14 @@ args = parser.parse_args()
 data_path = 'data/{}_velocity.h5ad'.format(args.subset)
 
 adata = sc.read(data_path)
-scv.pl.velocity_embedding_stream(adata, basis='umap', legend_fontsize=12, title='', smooth=.8, min_mass=4, color='type', 
+scv.pl.velocity_embedding_stream(adata, basis='umap', legend_fontsize=12, dpi=200, title='{} by type'.format(args.subset), color='type', 
 	save='{}_stream_type.png'.format(args.subset))
 
-scv.pl.velocity_embedding_stream(adata, basis='umap', legend_fontsize=12, title='', smooth=.8, min_mass=4, color='day', 
+scv.pl.velocity_embedding_stream(adata, basis='umap', legend_fontsize=12, dpi=200, title='{} by day'.format(args.subset), color='day', 
 	save='{}_stream_day.png'.format(args.subset))
 
-scv.pl.velocity_embedding(adata, basis='umap', arrow_length=2, arrow_size=2, dpi=200, color='type', 
+scv.pl.velocity_embedding(adata, basis='umap', dpi=200, density=0.75, color='type', 
 	save='{}_arrow_type.png'.format(args.subset))
 
-scv.pl.velocity_embedding(adata, basis='umap', arrow_length=2, arrow_size=2, dpi=200, color='day', 
+scv.pl.velocity_embedding(adata, basis='umap', dpi=200, density=0.75, color='day', 
 	save='{}_arrow_day.png'.format(args.subset))
