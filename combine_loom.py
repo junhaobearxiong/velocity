735d1a64b1858a455b403511297a728c8748d4c2
junhaobearxiong <jxiong3@jhu.edu>
2021-02-25 15:44:56 +0800

    E3 loom

--- combine_loom.py
+++ combine_loom.py
@@ -1,7 +1,7 @@
 import loompy
 
 input_dirname = 'velocyto_output/'
-output_path = 'data/E2.loom'
+output_path = 'data/E3.loom'
 
 '''
 files = [
@@ -26,6 +26,7 @@ files = [
 ]
 '''
 
+'''
 files = [
     'finalmerged_E2CD1col1_Aligned_LQF2W.loom',
     'finalmerged_E2CD1col2_Aligned_3JT6M.loom',
@@ -44,7 +45,29 @@ files = [
     'finalmerged_E2CD3col3_Aligned_8KXJE.loom',
     'finalmerged_E2CD3col4_Aligned_U67XA.loom',
     'finalmerged_E2CD3col5_Aligned_ZLNP3.loom',
-    'finalmerged_E2CD3col6_Aligned_4R8BF.loom'.
+    'finalmerged_E2CD3col6_Aligned_4R8BF.loom'
+]
+'''
+
+files = [
+    'finalmerged_E3CD1col1_Aligned_E25XO.loom',
+    'finalmerged_E3CD1col2_Aligned_6B6E2.loom',
+    'finalmerged_E3CD1col3_Aligned_T9113.loom',
+    'finalmerged_E3CD1col4_Aligned_ES2YG.loom',
+    'finalmerged_E3CD1col5_Aligned_VAUFJ.loom',
+    'finalmerged_E3CD1col6_Aligned_1YZ9P.loom',
+    'finalmerged_E3CD2col1_Aligned_HBZNN.loom',
+    'finalmerged_E3CD2col2_Aligned_6TWSS.loom',
+    'finalmerged_E3CD2col3_Aligned_VTMTW.loom',
+    'finalmerged_E3CD2col4_Aligned_UKQDQ.loom',
+    'finalmerged_E3CD2col5_Aligned_J5SXB.loom',
+    'finalmerged_E3CD2col6_Aligned_BNMHR.loom',
+    'finalmerged_E3CD3col1_Aligned_2G8YW.loom',
+    'finalmerged_E3CD3col2_Aligned_LAZ9A.loom',
+    'finalmerged_E3CD3col3_Aligned_1AUTO.loom',
+    'finalmerged_E3CD3col4_Aligned_H96EV.loom',
+    'finalmerged_E3CD3col5_Aligned_67AIU.loom',
+    'finalmerged_E3CD3col6_Aligned_0A243.loom',
 ]
 
 files = [input_dirname + f for f in files]
