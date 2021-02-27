0179d685124d5a9ddb62c4f996322faaa8e54659
junhaobearxiong <jxiong3@jhu.edu>
2021-02-27 10:42:11 +0800

    separate script for estimating root cells from plotting, to speed up plotting for latent time and velocity pseudotime

---
+++ estimate_root_cells.py
@@ -0,0 +1,16 @@
+import scanpy as sc
+import scvelo as scv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('subset', help='the subset of cells where we are performing the analysis, i.e. E1, E1col1, or full')
+args = parser.parse_args()
+
+scv.settings.verbosity = 3
+scv.settings.set_figure_params('scvelo')
+
+data_path = 'data/{}_velocity.h5ad'.format(args.subset)
+
+adata = sc.read(data_path)
+scv.tl.terminal_states(adata)
+adata.write(data_path)
