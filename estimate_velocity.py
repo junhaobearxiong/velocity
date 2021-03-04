7befde16b46ec01422291d93b3a4a049df5995f1
junhaobearxiong <jxiong3@jhu.edu>
2021-03-04 15:54:51 +0800

    allows different mode for velocity estimation, and projection

--- estimate_velocity.py
+++ estimate_velocity.py
@@ -9,10 +9,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('subset', help='the subset of cells where we are performing the analysis, i.e. E1, E1col1, or full')
+parser.add_argument('mode', nargs='?', default='dynamical', help='deterministic, stochastic or dynamical')
 args = parser.parse_args()
 
+if args.mode not in ['deterministic', 'stochastic', 'dynamical']:
+	raise ValueError('{} is not a velocity mode'.format(args.mode))
+
 input_path = 'data/{}_joint.h5ad'.format(args.subset)
-output_path = 'data/{}_velocity.h5ad'.format(args.subset)
+output_path = 'data/{}_{}_velocity.h5ad'.format(args.subset, args.mode)
 cache_dir = 'cached_files/{}'.format(args.subset)
 
 scv.settings.verbosity = 3
@@ -26,10 +30,13 @@ sc.tl.pca(adata)
 sc.pp.neighbors(adata, n_pcs=30, n_neighbors=30)
 scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
 
-c = scachepy.Cache(cache_dir)
-c.tl.recover_dynamics(adata, force=False, n_jobs=16)
+if args.mode == 'dynamical':
+	c = scachepy.Cache(cache_dir)
+	c.tl.recover_dynamics(adata, force=False, n_jobs=16)
+	scv.tl.velocity(adata, mode='dynamical')
+else:
+	scv.tl.velocity(adata, mode=args.mode)
 
-scv.tl.velocity(adata, mode='dynamical')
 scv.tl.velocity_graph(adata)
 sc.tl.umap(adata)
 
--- project_velocity.py
+++ project_velocity.py
@@ -4,24 +4,25 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('subset', help='the subset of cells where we are performing the analysis, i.e. E1, E1col1, or full')
-parser.add_argument('tkey', type=int, default=0, help='whether to use days as `tkey` when computing velocity graph')
+parser.add_argument('mode', nargs='?', default='dynamical', help='deterministic, stochastic or dynamical')
+parser.add_argument('tkey', nargs='?', type=int, default=0, help='whether to use days as `tkey` when computing velocity graph')
 args = parser.parse_args()
 
 if not args.tkey:
-    data_path = 'data/{}_velocity.h5ad'.format(args.subset)
+    data_path = 'data/{}_{}_velocity.h5ad'.format(args.subset, args.mode)
 else:
-    data_path = 'data/{}_velocity_tkey.h5ad'.format(args.subset)
+    data_path = 'data/{}_{}_velocity_tkey.h5ad'.format(args.subset, args.mode)
 
 adata = sc.read(data_path)
 
 scv.pl.velocity_embedding_stream(adata, basis='umap', legend_fontsize=12, dpi=200, title='{} by type'.format(args.subset), color='type', 
-    save='{}_stream_type_tkey{}.png'.format(args.subset, args.tkey))
+    save='{}_stream_type_{}_tkey{}.png'.format(args.subset, args.mode, args.tkey))
 
 scv.pl.velocity_embedding_stream(adata, basis='umap', legend_fontsize=12, dpi=200, title='{} by day'.format(args.subset), color='day', 
-    save='{}_stream_day_tkey{}.png'.format(args.subset, args.tkey))
+    save='{}_stream_day_{}_tkey{}.png'.format(args.subset, args.mode, args.tkey))
 
 scv.pl.velocity_embedding(adata, basis='umap', dpi=200, density=0.75, color='type', 
-    save='{}_arrow_type_tkey{}.png'.format(args.subset, args.tkey))
+    save='{}_arrow_type_{}_tkey{}.png'.format(args.subset, args.mode, args.tkey))
 
 scv.pl.velocity_embedding(adata, basis='umap', dpi=200, density=0.75, color='day', 
-    save='{}_arrow_day_tkey{}.png'.format(args.subset, args.tkey))
+    save='{}_arrow_day_{}_tkey{}.png'.format(args.subset, args.mode, args.tkey))
