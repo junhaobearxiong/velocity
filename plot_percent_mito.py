dcff1ed27846652ada68f2fec6eeab695585d89c
Junhao Xiong <junhaobearxiong@gmail.com>
2021-03-10 05:14:53 -0600

    add percent mito figs

--- plot_percent_mito.py
+++ plot_percent_mito.py
@@ -1,11 +1,11 @@
 import scanpy as sc
 import scvelo as scv
 
-adata = scv.read('data/E1col1_joint.h5ad', cache=True)
+adata = scv.read('data/full_joint.h5ad', cache=True)
 scv.utils.show_proportions(adata)
 scv.pl.proportions(adata, groupby='type')
 
 percent_mito_lowerbd = [0.1, 0.5, 1, 5]
 for i in percent_mito_lowerbd:
 	adata.obs['percent.mito.>{}'.format(i)] = (adata.obs['percent.mito'] > i).astype(int)
-	sc.pl.embedding(adata, 'umap', color=['percent.mito.>1'], dpi=150, save='_E1col1_pctmito_{}.png'.format(i))
+	sc.pl.embedding(adata, 'umap', color=['percent.mito.>{}'.format(i)], save='_full_pct_mito_{}.png'.format(i))
