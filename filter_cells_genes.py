daf73ec03d9af29135c95a2212aff93a8c56c67d
junhaobearxiong <jxiong3@jhu.edu>
2021-03-22 23:06:56 +0800

    add gene filters

--- filter_cells_genes.py
+++ filter_cells_genes.py
@@ -11,16 +11,19 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('subset', help='the subset of cells where we are performing the analysis, i.e. E1, E1col1, or full')
+# look up `args.filter` for the lines where each filter setting is applied
+parser.add_argument('filter', type=int, nargs='?', default=0, help='the type of cells/genes filter to use, 0 is the default setting of scvelo')
 args = parser.parse_args()
 
 input_exp_path = 'data/seurat.annotated.sct.h5ad'
 input_velo_path = 'data/{}.loom'.format(args.subset)
-output_path = 'data/{}_joint.h5ad'.format(args.subset)
+output_path = 'data/{}_filter{}_joint.h5ad'.format(args.subset, args.filter)
 
 scv.settings.verbosity = 3
 scv.settings.set_figure_params('scvelo')
 
 adata = scv.read(input_velo_path, cache=True)
+adata.var_names_make_unique()
 scv.utils.show_proportions(adata)
 print('velocyto data shape: {}'.format(adata.shape))
 adata_exp = sc.read(input_exp_path, cache=True)
@@ -48,8 +51,18 @@ adata_exp = adata_exp[joint_index]
 adata.obs = adata.obs.join(adata_exp.obs)
 adata.obs['day'] = adata.obs['diffday'].apply(lambda x: int(x[3:]))
 
-# preprocess data, compute pca and umap
-scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+# filter genes
+if args.filter == 0:
+	print('using default filter')
+	scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+elif args.filter == 1:
+	# also filter out genes expressed in < 10 cells, and genes on X, Y chromosomes
+	print('using filter 1')
+	scv.pp.filter_and_normalize(adata, min_shared_counts=20, min_shared_cells=10, n_top_genes=2000)
+	autosomal_genes = adata.var[~adata.var['Chromosome'].isin(['X', 'Y'])].index
+	adata = adata[:, autosomal_genes]
+
+# compute pca, neighbors, moments, umap
 sc.tl.pca(adata)
 sc.pp.neighbors(adata, n_pcs=30, n_neighbors=30)
 scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
